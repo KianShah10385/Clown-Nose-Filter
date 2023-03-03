0a85c3dce1707d1cb1578b76b660b729e5118136
KianShah10385 <108907854+KianShah10385@users.noreply.github.com>
2023-03-02 20:38:24 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preload() {
+}
+
+function setup() 
+{
+    canvas = createCanvas(300,300);
+    canvas.center();
+}
+
+function draw()
+{
+}
+function take_snapshot() {
+save('class123.png');
+}
